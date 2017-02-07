8dc44b1114ba7041d4ef95a04e64c71eb0d1f54d
Garrett Brown <themagnificentmrb@gmail.com>
2017-02-06 18:41:16 -0800

    Fix missing #include

--- xbmc/windowing/egl/EGLWrapper.cpp
+++ xbmc/windowing/egl/EGLWrapper.cpp
@@ -40,6 +40,7 @@
 #if HAVE_SDL_VERSION == 2
 #include "EGLNativeTypeSDL.h"
 #endif
+#include "EGLNativeType.h"
 #include "EGLWrapper.h"
 
 #define CheckError() m_result = eglGetError(); if(m_result != EGL_SUCCESS) CLog::Log(LOGERROR, "EGL error in %s: %x",__FUNCTION__, m_result);
